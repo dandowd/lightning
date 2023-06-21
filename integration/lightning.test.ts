89849df80b0599982b07df6d1f7ec91c41ff73ee
Dan Dowd <danieldowd89@gmail.com>
2023-06-21 12:19:54 -0600

    adds complete integration test

--- integration/lightning.test.ts
+++ integration/lightning.test.ts
@@ -1,6 +1,8 @@
+import fs from 'fs'
 import { spawn } from 'child_process'
 
 describe('it should read from stdin', () => {
+  jest.setTimeout(7000)
   let sut: ReturnType<typeof spawn>
 
   beforeEach(() => {
@@ -13,16 +15,20 @@ describe('it should read from stdin', () => {
   })
 
   it('should write struck area to stdout', (done) => {
-    const streamData = `{"flashType":1,"strikeTime":1446760903089,"latitude":33.7320437,"longitude":-96.7257525,"peakAmps":2613,"reserved":"000","icHeight":12287,"receivedTime":1446760915221,"numberOfSensors":7,"multiplicity":13}
-    {"flashType":1,"strikeTime":1446760903116,"latitude":33.7629128,"longitude":-96.6864843,"peakAmps":-2011,"reserved":"000","icHeight":13258,"receivedTime":1446760915221,"numberOfSensors":13,"multiplicity":13}
-    {"flashType":1,"strikeTime":1446760903180,"latitude":33.7373298,"longitude":-96.7196966,"peakAmps":17386,"reserved":"000","icHeight":19128,"receivedTime":1446760915221,"numberOfSensors":17,"multiplicity":13}`
+    const streamData = fs.readFileSync('./lightning.json')
 
+    const stdOut: string[] = []
     sut.stdout?.on('data', data => {
-      expect(data.toString()).toBe('lightning alert for 86315:Fahey Brooks\n')
-      done()
+      stdOut.push(data.toString())
     })
 
     sut.stdin?.setDefaultEncoding('utf-8')
     sut.stdin?.write(streamData)
+
+    // wait for process to be called
+    setTimeout(() => {
+      expect(stdOut).toMatchSnapshot()
+      done()
+    }, 6000)
   })
 })
