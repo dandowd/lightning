ece3c5b2c31fdb33a4d31d232e0eb54e6b2b8ffe
Dan Dowd <danieldowd89@gmail.com>
2023-06-21 11:59:56 -0600

    adds log dump

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { parseArgs } from 'node:util'
 import { getUniqueAssets } from './getUniqueAssets'
 import { loadAssets } from './loadAssets'
 import { validateLightningStrike } from './validateLightningStrike'
+import fs from 'fs'
 
 const { values: { assetsFile } } = parseArgs({
   args: process.argv,
@@ -22,6 +23,7 @@ if (assetsFile === undefined) {
 const assets = loadAssets(assetsFile)
 
 const dataFrame: string[] = []
+let logDump: any[] = []
 
 process.stdin.on('data', data => {
   const rows = data.toString().split('\n')
@@ -32,9 +34,9 @@ process.stdin.on('data', data => {
 
 const processData = (): void => {
   while (dataFrame.length > 0) {
-    try {
-      const row = dataFrame.pop()
+    const row = dataFrame.pop()
 
+    try {
       if (row === undefined) {
         continue
       }
@@ -48,8 +50,27 @@ const processData = (): void => {
       }
     } catch (err) {
       // log error
+      if (err instanceof Error) {
+        logDump.push({ row, err: err.message })
+      } else {
+        logDump.push({ row, err: 'unknown error' })
+      }
     }
   }
 }
+
+const dumpLogs = (): void => {
+  const dump = logDump
+  logDump = []
+
+  fs.appendFile('./log-dump', JSON.stringify(dump, null, 2), err => {
+    if (err !== null) {
+      console.error('error while dumping logs')
+      console.error(err)
+    }
+  })
+}
+
+setInterval(dumpLogs, 10000)
 
 setInterval(processData, 4000)
